3feabbb93437241c3eb221fcd1c7fbfabc38ed64
praveen-rangasubhe <praveentrangasubhe@gmail.com>
2022-09-22 22:11:35 +0530

    re stuctured

---
+++ rxjs/src/app/app-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { PromiseComponent } from './promise/promise.component';
+
+const routes: Routes = [
+  { path: 'promise', component: PromiseComponent },
+  { path: '**', redirectTo: 'promise' },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
